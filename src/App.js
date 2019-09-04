fc4244abb5f39158a0c5ff19b358140333cba09b
Matt Conway <mattconway727@gmail.com>
2019-09-03 20:54:57 -0400

    profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./css/App.css";
 import { Route } from "react-router-dom";
 import LoginRegForm from "./components/LoginRegForm";
 import DashBoard from "./components/DashBoard";
+import Profile from "./components/Profile";
 import "semantic-ui-css/semantic.min.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Route exact path="/" render={props => <LoginRegForm {...props} />} />
       <Route path="/dashboard" render={props => <DashBoard {...props} />} />
+      <Route path="/profile" render={props => <Profile {...props} />} />
     </div>
   );
 }
